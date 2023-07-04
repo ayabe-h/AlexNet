b831036cd1ac3217a9757ea6c0a990b26c0908ea
綾部響己 <106504927+ayabe-h@users.noreply.github.com>
2023-07-04 16:49:56 +0900

    Model改良

--- src/alexnet/model.py
+++ src/alexnet/model.py
@@ -5,34 +5,38 @@ class Model(nn.Module):
     """コンストラクタ"""
     def __init__(self):
         super(Model, self).__init__()
+        num_classes = 10
         # 特徴量抽出
         self.__features=nn.Sequential(
-            nn.Conv2d(3, 32, 3),
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, 3),
-            nn.MaxPool2d((2,2))
-        )
-        # 1次元データに変換
-        self.__flatten=nn.Sequential(
-            # 1次元データに変換
-            nn.Flatten()
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
         # 分類器
-        self.__classifier=nn.Sequential(
-            nn.Linear(6272, 128),
+        self.__classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 4 * 4, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(128, 10)
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
         )
-        
-    """順伝播"""
     def forward(self, x):
-        # 特徴量抽出
-        x=self.__features(x)
-        # 1次元データに変換
-        x=self.__flatten(x)
-        # 分類器
-        y=self.__classifier(x)
-        return y
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 4 * 4)
+        x = self.__classifier(x)
+        return x
     
     """特徴量"""
     def features(self, x):
